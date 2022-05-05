dcb8d0e69d1e3e8edcafa31a58188dbf414cadff
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-05 12:22:13 +0200

    ADD : gestion erreurs

--- testing/com/eni/test/CoffreFortTest.java
+++ testing/com/eni/test/CoffreFortTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
 
 import com.eni.vault.bll.BLLException;
 import com.eni.vault.bll.Vault;
+import com.eni.vault.bo.Gold;
 
 public class CoffreFortTest {
 	Vault vault = new Vault();
-	boolean statut;
-
+	Gold gold;
+	
 	@Test
 	public void fermeture() throws BLLException {
 		boolean excpected = true;
-		statut = false;
 		
 		String password = "motdepasse";
 
@@ -24,13 +24,44 @@ public class CoffreFortTest {
 		Assert.assertEquals(excpected, this.vault.isLocked());
 	}
 	
-	@Test
+	@Test (expected = BLLException.class)
+	public void fermetureDejaFerme() throws BLLException {
+		boolean excpected = true;
+		
+		String password = "motdepasse";
+		String newPassword = "123";
+
+		this.vault.lock(password);
+		this.vault.lock(newPassword);
+		
+		Assert.assertEquals(excpected, this.vault.isLocked());
+	}
+	
+	@Test (expected = BLLException.class)
 	public void ouverture() throws BLLException {
 		boolean excpected = false;
-		String password = "mauvaisMdp";
+		String password = "motdepasse";
 		this.vault.lock("motdepasse");
 		this.vault.unlock(password);
 		
 		Assert.assertEquals(excpected, this.vault.isLocked());
+		
+		String password2 = "mauvaisMdp";
+		//Commenter ligne suivante pour levée d'expection
+		this.vault.lock("motdepasse");
+		this.vault.unlock(password2);
+		Assert.assertNotEquals(excpected, this.vault.isLocked());
+		
+		
+		
+	}
+	
+	@Test
+	public void ajouter() throws BLLException {
+		int excpected = 1;
+		//Décommenter ligne suivante pour lever d'exception
+//		this.vault.lock("motdepasse");
+		
+		Assert.assertEquals(excpected, this.vault.addObject(gold));
 	}
 }
