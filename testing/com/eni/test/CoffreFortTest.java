fdb43c49e98d5bb378fd4cd01d13a081af04a239
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-05 13:26:09 +0200

    ADD : tous tests fonctionneels

--- testing/com/eni/test/CoffreFortTest.java
+++ testing/com/eni/test/CoffreFortTest.java
@@ -1,7 +1,5 @@
 package com.eni.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -37,7 +35,7 @@ public class CoffreFortTest {
 		Assert.assertEquals(excpected, this.vault.isLocked());
 	}
 	
-	@Test (expected = BLLException.class)
+	@Test
 	public void ouverture() throws BLLException {
 		boolean excpected = false;
 		String password = "motdepasse";
@@ -45,23 +43,54 @@ public class CoffreFortTest {
 		this.vault.unlock(password);
 		
 		Assert.assertEquals(excpected, this.vault.isLocked());
-		
-		String password2 = "mauvaisMdp";
-		//Commenter ligne suivante pour levée d'expection
-		this.vault.lock("motdepasse");
-		this.vault.unlock(password2);
-		Assert.assertNotEquals(excpected, this.vault.isLocked());
-		
-		
-		
 	}
+	
+	@Test
+	public void ouvertureWPW() throws BLLException {
+		boolean excpected = false;
+		String password = "mauvaisMdp";
+		this.vault.lock("motdepasse");
+		this.vault.unlock(password);
+		Assert.assertNotEquals(excpected, this.vault.isLocked());
+	}	
+		
+	@Test (expected = BLLException.class)
+	public void ouvertureDejaOuvert() throws BLLException {
+		boolean excpected = false;
+		String password = "password";
+		this.vault.unlock(password);
+		Assert.assertNotEquals(excpected, this.vault.isLocked());
+	}	
 	
 	@Test
 	public void ajouter() throws BLLException {
 		int excpected = 1;
-		//Décommenter ligne suivante pour lever d'exception
-//		this.vault.lock("motdepasse");
 		
 		Assert.assertEquals(excpected, this.vault.addObject(gold));
+	}
+	
+	@Test (expected = BLLException.class)
+	public void ajouterCoffreFerme() throws BLLException {
+		int excpected = 1;
+		this.vault.lock("motdepasse");
+		Assert.assertEquals(excpected, this.vault.addObject(gold));
+	}
+	
+	@Test
+	public void enlever() throws BLLException {
+		int excpected = 0;
+		
+		this.vault.addObject(gold);
+		Assert.assertEquals(excpected, this.vault.removeObject(gold));
+	}
+	
+	@Test (expected = BLLException.class)
+	public void enleverCoffreFerme() throws BLLException {
+		int excpected = 0;
+		
+		this.vault.addObject(gold);
+		this.vault.lock("motdepasse");
+		this.vault.lock("motdepasse");
+		Assert.assertEquals(excpected, this.vault.removeObject(gold));
 	}
 }
